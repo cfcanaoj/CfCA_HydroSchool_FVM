307f68353b1093243dfb6d5bd9ab07b9949d9c4c
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2026-02-09 19:33:01 +0900

    animate => animation

--- Radiation/1D_Diffusion/MakeAnime.sh
+++ Radiation/1D_Diffusion/MakeAnime.sh
@@ -31,7 +31,7 @@ fstnum=$(basename "$fstfile" | tr -cd '0-9\n' | sed -e 's/^0\+\([0-9]\+\)$/\1/')
 echo "$fstnum"
 
 echo "wmv format"
-ffmpeg -y -r 10 -start_number "$fstnum" -i "${dir}/${prename}%5d.png" -b 6000k -vcodec wmv2 -pass 1 -r 10 -an "${dir}/animate.wmv"
+ffmpeg -y -r 10 -start_number "$fstnum" -i "${dir}/${prename}%5d.png" -b 6000k -vcodec wmv2 -pass 1 -r 10 -an "${dir}/animation.wmv"
 
 echo "mp4 format"
-ffmpeg -y -r 10 -start_number "$fstnum" -i "${dir}/${prename}%5d.png" -vcodec libx264 -pix_fmt yuv420p -r 10 -an "${dir}/animate.mp4"
+ffmpeg -y -r 10 -start_number "$fstnum" -i "${dir}/${prename}%5d.png" -vcodec libx264 -pix_fmt yuv420p -r 10 -an "${dir}/animation.mp4"
