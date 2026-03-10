bbe3de133fdcedc7ca2a4c2a164e18334f5a1d50
Kazunari Iwasaki <kazunari.iwasaki@gmail.com>
2026-03-10 17:52:59 +0900

    add python scripts for visualization

---
+++ multiD/AlfvenWave/MakeAnime.py
@@ -0,0 +1,67 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import re
+
+def read_textdata(file):
+    with open(file, "r") as f:
+        time = float(f.readline().split()[-1])
+        nx, ny = map(int, f.readline().split()[-2:])
+
+    arr = np.loadtxt(file).reshape(ny, nx, -1)
+
+    x = arr[:, :, 0]
+    y = arr[:, :, 1]
+
+    names = ["rho", "vx", "vy", "vz", "pre", "Bx", "By", "Bz"]
+    data_dict = {name: arr[:, :, i+2] for i, name in enumerate(names)}
+
+    return x, y, time, data_dict
+
+dirname = sys.argv[1]
+step_s = int(sys.argv[2])
+step_e = int(sys.argv[3])
+
+xmin = 0.0
+xmax = 1.0
+ymin = 0.0
+ymax = 0.5
+
+fig_width = 6.4
+fig_height = fig_width*(ymax-ymin)/(xmax-xmin) + 1.9
+fig = plt.figure(figsize=(fig_width, fig_height)) 
+
+plt.xlim(xmin,xmax)
+plt.ylim(ymin,ymax)
+plt.xlabel("x axis") 
+plt.ylabel("y axis") 
+
+fname_anime = "animation.mp4"
+
+graph_list = [] 
+for istep in range(step_s,step_e+1):
+    foutname = dirname + "/snap%05d.dat"%(istep) 
+
+    print("making plot ",foutname)
+    x, y, time, data = read_textdata(foutname)
+
+    pg00 = plt.text(0.5*(xmin+xmax),ymax*1.1,r"$\mathrm{time}=%.2f$"%(time),horizontalalignment="center")
+
+    im=plt.imshow(data['Bz'],extent=(xmin,xmax,ymin,ymax),origin="lower",vmin=-0.1,vmax=0.1)
+
+    if istep == step_s: 
+        cbar = plt.colorbar(im,orientation="horizontal")
+        cbar.set_label(r"$Bz$")
+    graph_list.append([pg00,im])               
+
+ani = animation.ArtistAnimation(fig, graph_list, interval=200) 
+print("making animation file", fname_anime)
+ani.save(
+    dirname + "/" + fname_anime,
+    writer="ffmpeg",
+    dpi=150,
+    codec="libx264",
+    extra_args=["-pix_fmt", "yuv420p", "-vf", "scale=trunc(iw/2)*2:trunc(ih/2)*2"]
+)
+plt.show()
---
+++ multiD/AlfvenWave/MakePlot_binary.py
@@ -0,0 +1,75 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+def read_bindata(file):
+    with open(file, "rb") as fp: 
+        time = np.fromfile(fp, np.float64, 1).item() 
+        nx, ny, nhyd, nbc = [np.fromfile(fp, np.int32, 1).item() for _ in range(4)]
+
+        xv = np.fromfile(fp, np.float64, nx)
+        yv = np.fromfile(fp, np.float64, ny)
+        Q  = np.fromfile(fp, np.float32, nx * ny * nhyd).reshape(ny, nx, nhyd)
+        Bc = np.fromfile(fp, np.float32, nx * ny * nbc ).reshape(ny, nx, nbc)
+
+    q_names = ['rho', 'vx', 'vy', 'vz', 'pre']
+    b_names = ['Bx', 'By', 'Bz']
+
+    data_dict = {name: Q[:, :, i]  for i, name in enumerate(q_names)}
+    data_dict.update({name: Bc[:, :, i] for i, name in enumerate(b_names)})
+
+    return xv, yv, time, data_dict
+
+def vecpot(x,y,Bx,By):
+    Az = np.zeros_like(By)
+    dy = y[1] - y[0]
+    dx = x[1] - x[0]
+    
+    Az[1:, 0] = Az[0, 0] + np.cumsum(0.5*(Bx[1:, 0] + Bx[:-1, 0])*dy)
+    Az[:, 1:] = Az[:, [0]] - np.cumsum(0.5*(By[:, 1:] + By[:, :-1])*dx, axis=1)
+    
+    return Az
+
+###############################
+dirname = sys.argv[1]
+step = int(sys.argv[2])
+
+foutname = dirname + "/snap%05d.bin"%(step) 
+x, y, time, data = read_bindata(foutname)
+Az = vecpot(x,y,data['Bx'],data['By'])
+
+xmin = 0.0
+xmax = 1.0
+ymin = 0.0
+ymax = 0.5
+
+fig_width = 6.4
+fig_height = fig_width*(ymax-ymin)/(xmax-xmin) + 1.9
+fig = plt.figure(figsize=(fig_width, fig_height)) 
+
+plt.xlim(xmin,xmax)
+plt.ylim(ymin,ymax)
+plt.xlabel("x axis") 
+plt.ylabel("y axis") 
+
+plt.text(0.5*(xmin+xmax),ymax*1.1,r"$\mathrm{time}=%.2f$"%(time),horizontalalignment="center")
+
+im=plt.imshow(data['Bz'],extent=(xmin,xmax,ymin,ymax),origin="lower",vmin=-0.1,vmax=0.1)
+cbar = plt.colorbar(im,orientation="horizontal")
+cbar.set_label(r"$B_z$")
+
+#yy, xx = np.meshgrid(y,x,indexing="ij")
+#plt.contour(xx,yy,Az,linestyles='solid',levels=20,colors="white")
+
+for format_fig in ["pdf","png"]:
+    outdir = dirname + "/" + format_fig + "file"
+    os.makedirs(outdir, exist_ok=True)
+
+    outputfile = outdir + "/snap%05d."%(step) + format_fig
+
+    print("making plot file", outputfile)
+    plt.savefig(outputfile,bbox_inches="tight")
+
+plt.show()
