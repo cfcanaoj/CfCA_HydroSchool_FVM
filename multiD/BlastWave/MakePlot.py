07b7953db2ccf06ae64bf5be39a15cd691e3d649
Kazunari Iwasaki <kazunari.iwasaki@gmail.com>
2026-03-12 17:47:00 +0900

    asciiとbinaryファイル両方読み込めるようにした

--- multiD/BlastWave/MakePlot.py
+++ multiD/BlastWave/MakePlot.py
@@ -18,25 +18,53 @@ def read_textdata(file):
     names = ["rho", "vx", "vy", "vz", "pre", "Bx", "By", "Bz"]
     data_dict = {name: arr[:, :, i+2] for i, name in enumerate(names)}
 
-    return x, y, time, data_dict
+    return x[0,:], y[:,0], time, data_dict
+
+def read_bindata(file):
+    with open(file, "rb") as fp: 
+        time = np.fromfile(fp, np.float64, 1).item() 
+        nx, ny, nhyd, nbc = [np.fromfile(fp, np.int32, 1).item() for _ in range(4)]
+
+        xv = np.fromfile(fp, np.float64, nx)
+        yv = np.fromfile(fp, np.float64, ny)
+        Q  = np.fromfile(fp, np.float32, nx * ny * nhyd).reshape(ny, nx, nhyd)
+        Bc = np.fromfile(fp, np.float32, nx * ny * nbc ).reshape(ny, nx, nbc)
+
+    q_names = ['rho', 'vx', 'vy', 'vz', 'pre']
+    b_names = ['Bx', 'By', 'Bz']
+
+    data_dict = {name: Q[:, :, i]  for i, name in enumerate(q_names)}
+    data_dict.update({name: Bc[:, :, i] for i, name in enumerate(b_names)})
+
+    return xv, yv, time, data_dict
 
 def vecpot(x,y,Bx,By):
     Az = np.zeros_like(By)
-    dy = y[1,0] - y[0,0]
-    dx = x[0,1] - x[0,0]
+    dy = y[1] - y[0]
+    dx = x[0] - x[0]
     
     Az[1:, 0] = Az[0, 0] + np.cumsum(0.5*(Bx[1:, 0] + Bx[:-1, 0])*dy)
     Az[:, 1:] = Az[:, [0]] - np.cumsum(0.5*(By[:, 1:] + By[:, :-1])*dx, axis=1)
     
     return Az
 
-dirname = sys.argv[1]
-step = int(sys.argv[2])
+filetype = sys.argv[1]
+dirname = sys.argv[2]
+step = int(sys.argv[3])
 
-foutname = dirname + "/snap%05d.dat"%(step) 
-print("making plot ",foutname)
 
-x, y, time, data = read_textdata(foutname)
+if filetype == "ascii": 
+    foutname = dirname + "/snap%05d.dat"%(step) 
+    print("making plot ",foutname)
+    x, y, time, data = read_textdata(foutname)
+elif filetype == "binary":
+    foutname = dirname + "/snap%05d.bin"%(step) 
+    print("making plot ",foutname)
+    x, y, time, data = read_bindata(foutname)
+else:
+    print("error! filetype should be ascii or binary")
+    exit(-1)
+
 Az = vecpot(x,y,data['Bx'],data['By'])
 
 xmin = -0.5
