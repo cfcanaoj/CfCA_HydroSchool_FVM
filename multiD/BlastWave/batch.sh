facad2a2bdcffe8396d95c8df0bc49bc2c7bc108
Kazunari Iwasaki <kazunari.iwasaki@gmail.com>
2026-03-18 00:03:14 +0900

    job script file

---
+++ multiD/BlastWave/batch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=blast
+#SBATCH --partition=M-large-t
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --threads-per-core=1
+#SBATCH --mem=30G
+#SBATCH -o %x_%J.out
+#SBATCH -e %x_%J.out
+#SBATCH --hint=nomultithread
+##SBATCH --reservation=hydro
+
+### the number of threads (can be modified)
+#SBATCH --cpus-per-task=32
+
+module switch PrgEnv-cray PrgEnv-intel
+module list
+
+export KMP_AFFINITY=compact
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK} 
+
+cd $SLURM_SUBMIT_DIR
+
+srun -c ${SLURM_CPUS_PER_TASK} ./a.out
